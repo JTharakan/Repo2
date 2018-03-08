00281d83177df7c534c04424dfefa61531dcac04
Tharakan <jacob.tharakan@hpe.com>
2018-03-08 17:12:37 +0530

    modified maven project

--- src/test/java/com/bd/maven/NewProject/AppTest.java
+++ src/test/java/com/bd/maven/NewProject/AppTest.java
@@ -35,4 +35,18 @@ public class AppTest
     {
         assertTrue( true );
     }
+    
+    public void simpleTestPass(){
+    	int a = 1;
+    	int b = 2;
+    	assertTrue(a+b==3);
+    
+    }
+    
+    public void simpleTestFail(){
+    	int a = 2;
+    	int b = 2;
+    	assertTrue(a+b==5);
+    
+    }
 }
